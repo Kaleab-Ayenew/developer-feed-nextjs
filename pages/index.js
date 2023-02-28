0ad10a4ed817336b2494ff65fe977ccea29f6577
Kaleab-Ayenew <kaleab.ayenew2013@gmail.com>
2023-02-28 22:07:54 +0300

    Changed the favicon to a png

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ feeds }) {
           content="A developer news feed that fetches latest, trending news from medium."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <link rel="manifest" href="/manifest.json"></link>
       </Head>
       <NextSeo
